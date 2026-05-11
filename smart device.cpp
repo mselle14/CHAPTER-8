9f78a4dd530a9a5ebdd676e49e9290839979be2f
mselle14 <mselle14@aum.edu>
2026-05-10 21:56:35 -0500

    Update smart device.cpp

--- smart device.cpp
+++ smart device.cpp
@@ -1,20 +1,30 @@
-// smart device.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//Marshon Sellers
 #include <iostream>
+using namespace std;
+
+enum PowerState
+{
+    Off,
+    On,
+    Sleep
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    PowerState device_state = Sleep;
+
+    switch (device_state)
+    {
+    case Off:
+        cout << "Device is turned off";
+        break;
+    case On:
+        cout << "Device is running";
+        break;
+    case Sleep:
+        cout << "Device is in power-saving mode";
+        break;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
